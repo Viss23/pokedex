efb8c76ae80446f90adc79140ec787799fb8dfcd
viss23 <wissper777@gmail.com>
2020-10-22 16:18:32 +0300

    Updates

---
+++ precache-manifest.23963eb74fc31444d04c337bdf090f66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e350d405de9a0efb23ffd6b0887bb6f4",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "0518c387d965ad483e32",
+    "url": "/pokedex/static/css/main.932ff0c7.chunk.css"
+  },
+  {
+    "revision": "9a36b1c79bd553106699",
+    "url": "/pokedex/static/js/2.351d787c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/pokedex/static/js/2.351d787c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0518c387d965ad483e32",
+    "url": "/pokedex/static/js/main.e7ee944c.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
